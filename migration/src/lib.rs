7181dc6c2a21f14381c56cfcade3a3d6c11669ff
David Badalyan <badalyandavo@gmail.com>
2023-01-13 14:36:13 +0800

    Set up migrations

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,24 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
+mod m20230113_143230_init;
+mod m20230113_143331_create_currency_table;
+mod m20230113_143340_create_account_table;
+mod m20230113_143352_create_forex_table;
+mod m20230113_143410_create_transaction_table;
+mod m20230113_143431_create_transfer_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_table::Migration)]
+        vec![
+            Box::new(m20230113_143230_init::Migration),
+            Box::new(m20230113_143331_create_currency_table::Migration),
+            Box::new(m20230113_143340_create_account_table::Migration),
+            Box::new(m20230113_143352_create_forex_table::Migration),
+            Box::new(m20230113_143410_create_transaction_table::Migration),
+            Box::new(m20230113_143431_create_transfer_table::Migration),
+        ]
     }
 }
