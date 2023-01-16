ee078e56963fd8fb3f5cfbc014ac3603dd8fda02
David Badalyan <badalyandavo@gmail.com>
2023-01-16 18:22:55 +0800

    Load .env file in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #![allow(unused)]
 // TODO: Run the main function of api here so playground can be accessed
 
+#[cfg(debug_assertions)]
+use dotenvy::dotenv;
+
 fn main() {
-    accountant_graphql_api::main();
+    #[cfg(debug_assertions)]
+    dotenv().ok();
+
+    accountant_graphql_api::serve();
 }
