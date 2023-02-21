bec7262b2ecaa9476c8ef386ad9b2239068c54a8
David Badalyan <badalyandavo@gmail.com>
2023-02-21 14:26:02 +0400

    Remove leftover methods

--- core/src/query.rs
+++ core/src/query.rs
@@ -1,6 +1,7 @@
 use ::entity::{account, account::Entity as Account};
 use ::entity::{currency, currency::Entity as Currency};
 use sea_orm::*;
+
 pub struct Query;
 impl Query {
     pub async fn get_all_accounts(db: &DbConn) -> Result<Vec<account::Model>, DbErr> {
@@ -14,14 +15,5 @@ impl Query {
     }
     pub async fn get_transactions(db: &DbConn) -> Result<Vec<account::Model>, DbErr> {
         Account::find().all(db).await
-    }
-    pub async fn get_all_currencies(db: &DbConn) -> Result<Vec<currency::Model>, DbErr> {
-        Currency::find().all(db).await
-    }
-    pub async fn find_currency_by_id(
-        db: &DbConn,
-        id: i32,
-    ) -> Result<Option<currency::Model>, DbErr> {
-        Currency::find_by_id(id).one(db).await
     }
 }
