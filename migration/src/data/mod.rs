d6461a9cbc894970a19618a14abcef76e96651af
David Badalyan <badalyandavo@gmail.com>
2023-03-09 20:59:26 +0400

    Add function to load categories

--- migration/src/data/mod.rs
+++ migration/src/data/mod.rs
@@ -2,3 +2,8 @@ pub fn get_currencies_str() -> &'static str {
     //TODO: Parse the contents and only include bytes that are parsed
     return include_str!("currencies.json");
 }
+
+pub fn get_categories_str() -> &'static str {
+    //TODO: Parse the contents and only include bytes that are parsed
+    return include_str!("categories.json");
+}
