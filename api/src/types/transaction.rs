c6ce1e10d992eb40952a889a34df807f1e520ca2
David Badalyan <badalyandavo@gmail.com>
2023-04-15 20:56:48 +0400

    WIP: Create transaction gql types

--- api/src/types/transaction.rs
+++ api/src/types/transaction.rs
@@ -1,1 +1,25 @@
+use entity::transaction;
+use juniper::{graphql_interface, graphql_object, ID};
 
+#[graphql_interface]
+trait Transaction {
+    fn id(&self) -> ID;
+}
+
+#[derive(Debug, Clone)]
+pub struct Income {
+    model: transaction::Model,
+}
+
+impl From<transaction::Model> for Income {
+    fn from(value: transaction::Model) -> Self {
+        Income { model: value }
+    }
+}
+
+#[graphql_object(impl = TransactionValue)]
+impl Transaction for Income {
+    fn id(&self) -> ID {
+        ID::from(self.model.id.to_string())
+    }
+}
