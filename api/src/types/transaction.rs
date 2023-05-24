e2306c0beab31cdb2823c92ffe8b6757ea526889
David Badalyan <badalyandavo@gmail.com>
2023-05-24 19:39:30 +0400

    WIP: Fix transactions gql types with union

--- api/src/types/transaction.rs
+++ api/src/types/transaction.rs
@@ -1,39 +1,45 @@
+use crate::{context::Context, types::ConvertableResult};
+use accountant_core::account;
+use chrono::{DateTime, Utc};
 use cuid::cuid2 as cuid;
+use derive_more::From;
 use entity::transaction;
-use juniper::{graphql_interface, graphql_object, GraphQLEnum, GraphQLInputObject, ID};
+use juniper::{graphql_object, FieldResult, GraphQLInputObject, GraphQLUnion, ID};
 
-#[graphql_interface]
-pub trait Transaction {
-    fn id(&self) -> ID;
-    fn date(&self) -> &String;
-    fn amount(&self) -> &f64;
-    fn description(&self) -> Option<&String>;
-}
+use crate::types::Account;
 
 #[derive(Debug, Clone)]
-pub struct Income {
+pub struct DebitTransaction {
     model: transaction::Model,
 }
-
-#[derive(GraphQLEnum)]
-pub enum TransactionType {
-    Income,
-    Expense,
+#[derive(Debug, Clone)]
+pub struct CreditTransaction {
+    model: transaction::Model,
 }
 
 #[derive(GraphQLInputObject)]
 pub struct NewTransactionInput {
-    pub r#type: TransactionType,
     pub date: String,
     pub amount: f64,
     pub description: String,
     pub account_id: ID,
     pub category_id: ID,
 }
+#[derive(GraphQLInputObject)]
+pub struct TimespanInput {
+    pub from: f64,
+    pub to: f64,
+}
 
-impl From<transaction::Model> for Income {
+impl From<transaction::Model> for DebitTransaction {
     fn from(value: transaction::Model) -> Self {
-        Income { model: value }
+        DebitTransaction { model: value }
+    }
+}
+
+impl From<transaction::Model> for CreditTransaction {
+    fn from(value: transaction::Model) -> Self {
+        CreditTransaction { model: value }
     }
 }
 
@@ -43,15 +49,19 @@ impl From<NewTransactionInput> for transaction::Model {
             id: cuid(),
             account_id: value.account_id.to_string(),
             amount: value.amount,
-            created_at: "0".to_owned(),
-            updated_at: "0".to_owned(),
             notes: Some(value.description),
+            date: DateTime::parse_from_rfc3339(&value.date)
+                .unwrap()
+                .with_timezone(&Utc)
+                .to_string(),
+            created_at: Utc::now().to_string(),
+            updated_at: Utc::now().to_string(),
         }
     }
 }
 
-#[graphql_object(impl = TransactionValue)]
-impl Transaction for Income {
+#[graphql_object(context = Context)]
+impl DebitTransaction {
     fn id(&self) -> ID {
         ID::from(self.model.id.to_string())
     }
@@ -63,5 +73,53 @@ impl Transaction for Income {
     }
     fn description(&self) -> &Option<String> {
         &self.model.notes
+    }
+    async fn toAccount(&self, context: &Context) -> FieldResult<Account> {
+        let conn = context.get_connection();
+        let account =
+            account::AccountQuery::find_account_by_id(conn, self.model.account_id.to_string())
+                .await;
+
+        account.convert()
+    }
+}
+
+#[graphql_object(context = Context)]
+impl CreditTransaction {
+    fn id(&self) -> ID {
+        ID::from(self.model.id.to_string())
+    }
+    fn date(&self) -> &String {
+        &self.model.created_at
+    }
+    fn amount(&self) -> &f64 {
+        &self.model.amount
+    }
+    fn description(&self) -> &Option<String> {
+        &self.model.notes
+    }
+    async fn fromAccount(&self, context: &Context) -> FieldResult<Account> {
+        let conn = context.get_connection();
+        let account =
+            account::AccountQuery::find_account_by_id(conn, self.model.account_id.to_string())
+                .await;
+
+        account.convert()
+    }
+}
+
+#[derive(From, GraphQLUnion)]
+#[graphql(context = Context)]
+pub enum Transaction {
+    Debit(DebitTransaction),
+    Credit(CreditTransaction),
+}
+
+impl From<transaction::Model> for Transaction {
+    fn from(value: transaction::Model) -> Self {
+        match value.amount > 0.0f64 {
+            true => Transaction::Debit(DebitTransaction::from(value)),
+            false => Transaction::Credit(CreditTransaction::from(value)),
+        }
     }
 }
