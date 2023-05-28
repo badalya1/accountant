084579d313bdd0092bb0eb9fc8c9a78b9f6fda89
David Badalyan <badalyandavo@gmail.com>
2023-05-27 23:40:15 -0800

    Implement transaction create

--- api/src/graphql/transaction/mutation.rs
+++ api/src/graphql/transaction/mutation.rs
@@ -2,16 +2,20 @@ use juniper::{graphql_object, FieldResult};
 
 use crate::context::Context;
 use crate::types::NewTransactionInput;
-use crate::types::{ConvertableResult, Income};
+use crate::types::{ConvertableResult, Transaction};
 use accountant_core::transaction;
 
 pub struct TransactionMutation;
 
 #[graphql_object(context = Context)]
 impl TransactionMutation {
-    async fn create(&self, context: &Context, data: NewTransactionInput) -> FieldResult<Income> {
+    async fn create(
+        &self,
+        context: &Context,
+        data: NewTransactionInput,
+    ) -> FieldResult<Transaction> {
         let conn = context.get_connection();
-        let new_transaction: Result<entity::transaction::Model, migration::DbErr> =
+        let new_transaction =
             transaction::TransactionMutation::create_transaction(conn, data.into()).await;
         new_transaction.convert()
     }
