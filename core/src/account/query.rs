810dc9a825fc41f1ff850cf4fe233fe7e2e2f138
David Badalyan <badalyandavo@gmail.com>
2023-05-28 20:28:18 +0400

    Implement core for transactions

--- core/src/account/query.rs
+++ core/src/account/query.rs
@@ -1,4 +1,5 @@
 use ::entity::{account, account::Entity as Account};
+use ::entity::{transaction, transaction::Entity as Transaction};
 use sea_orm::*;
 
 pub struct AccountQuery;
@@ -7,10 +8,22 @@ impl AccountQuery {
     pub async fn get_all_accounts(db: &DbConn) -> Result<Vec<account::Model>, DbErr> {
         Account::find().all(db).await
     }
-    pub async fn find_account_by_id(
-        db: &DbConn,
-        id: String,
-    ) -> Result<Option<account::Model>, DbErr> {
-        Account::find_by_id(id).one(db).await
+    pub async fn find_account_by_id(db: &DbConn, id: String) -> Result<account::Model, DbErr> {
+        let account = Account::find_by_id(id)
+            .one(db)
+            .await
+            .expect("Could not execute query on account");
+
+        account.ok_or(DbErr::RecordNotFound("Account not found".to_owned()))
+    }
+    pub async fn get_account_balance(db: &DbConn, id: String) -> Result<f64, DbErr> {
+        let transactions = Transaction::find()
+            .filter(transaction::Column::AccountId.eq(id))
+            .all(db)
+            .await?;
+
+        let balance = transactions.iter().fold(0.0, |acc, t| acc + t.amount);
+
+        Ok(balance)
     }
 }
--- core/src/transaction/query.rs
+++ core/src/transaction/query.rs
@@ -1,10 +1,17 @@
 use ::entity::{transaction, transaction::Entity as Transaction};
-use sea_orm::*;
+use sea_orm::{prelude::ChronoDateTimeUtc, *};
 
 pub struct TransactionQuery;
 
 impl TransactionQuery {
-    pub async fn get_all_transactions(db: &DbConn) -> Result<Vec<transaction::Model>, DbErr> {
-        Transaction::find().all(db).await
+    pub async fn get_all_transactions(
+        db: &DbConn,
+        from: ChronoDateTimeUtc,
+        to: ChronoDateTimeUtc,
+    ) -> Result<Vec<transaction::Model>, DbErr> {
+        Transaction::find()
+            .filter(transaction::Column::Date.between(from, to))
+            .all(db)
+            .await
     }
 }
